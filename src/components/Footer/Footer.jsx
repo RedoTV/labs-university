c24f6497b02acdbbf29050b7fae3df82af31e7d6
RedoTV <redotv1@gmail.com>
2024-09-14 18:04:03 +0300

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <p>Copyright</p>
+        <p>© 2024 Зинович Вадим Александрович </p>
+        <p>Контактный email: <a href="mailto:vadimzinovich06@gmail.com">vadimzinovich06@gmail.com</a></p>
       </footer>
     </>
   )
